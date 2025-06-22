624b044469cfc632f9f063b8603d401e0d4a6afe
Mohammad Fallah <mohammad@shape.dk>
2025-06-22 12:45:22 +0200

    Bring the story ring to the center

--- TMessagesProj/src/main/java/org/telegram/ui/Stories/ProfileStoriesView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Stories/ProfileStoriesView.java
@@ -489,8 +489,9 @@ public class ProfileStoriesView extends View implements NotificationCenter.Notif
         float avatarPullProgress = Utilities.clamp((avatarContainer.getScaleX() - 1f) / 0.4f, 1f, 0f);
         float insetMain = AndroidUtilities.lerp(AndroidUtilities.dpf2(4f), AndroidUtilities.dpf2(3.5f), avatarPullProgress);
         insetMain *= progressToInsets;
-        float ax = avatarContainer.getX() + insetMain * avatarContainer.getScaleX();
-        float ay = avatarContainer.getY() + insetMain * avatarContainer.getScaleY();
+        // COMMENT: Bring the story ring to the center
+        float ax = avatarContainer.getX() + insetMain * avatarContainer.getScaleX() - (((avatarContainer.getWidth() * avatarContainer.getScaleX()) - avatarContainer.getWidth()) / 2);
+        float ay = avatarContainer.getY() + insetMain * avatarContainer.getScaleY() - (((avatarContainer.getHeight() * avatarContainer.getScaleY()) - avatarContainer.getHeight()) / 2);
         float aw = (avatarContainer.getWidth() - insetMain * 2) * avatarContainer.getScaleX();
         float ah = (avatarContainer.getHeight() - insetMain * 2) * avatarContainer.getScaleY();
         rect1.set(ax, ay, ax + aw, ay + ah);
